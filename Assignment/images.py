e896aa0bd7d3db7a05d8eacf728f4e2f1cdf8e1b
Malte Wegener <malte.wegener99@gmx.de>
2019-03-18 12:38:28 +0100

    added noise to datasets

--- Assignment/images.py
+++ Assignment/images.py
@@ -2,12 +2,14 @@ import requests
 import random
 import urllib.request
 import re
+import shutil
+from random import shuffle
 
 query = "civil aircraft"
 
 r = requests.get("https://api.qwant.com/api/search/images",
     params={
-        'count': 120,
+        'count': 250,
         'q': query,
         't': 'images',
         'safesearch': 1,
@@ -20,18 +22,23 @@ r = requests.get("https://api.qwant.com/api/search/images",
 )
 
 response = r.json().get('data').get('result').get('items')
-print(response);
+print(response)
 urls = [r.get('media') for r in response]
 
-i = 0
-for url in urls:
-    i += 1
-    name = "A" + str(i)
-    print(i)
-    try:
-        urllib.request.urlretrieve(url, "pics/" + name+url[-4:])
-    except:
-        i -= 1
-    if i == 120:
-        exit()
+for j in range(0, 50):
+    shuffle(urls)
 
+    i = 0
+    for url in urls:
+        i += 1
+        name = "A" + str(i)
+        print(i)
+        try:
+            urllib.request.urlretrieve(url, "pics/" + name+url[-4:])
+        except:
+            i -= 1
+        if i == 120:
+            exit()
+
+    shutil.make_archive('pics'+str(j), 'zip')
+    print(j)
