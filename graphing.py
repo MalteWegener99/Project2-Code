de45e2427108b089951e5cbd23513f9af3cc4ed3
MalteWegener <malte.wegener99@gmx.de>
2019-03-21 13:11:59 +0300

    implemented local up

--- graphing.py
+++ graphing.py
@@ -4,6 +4,7 @@ import numpy as np
 import sys
 import struct
 import datetime
+from  math import cos, sin
 
 def parse_binary_llh(path):
     name = path.split('/')[-1][0:4]
@@ -16,7 +17,6 @@ def parse_binary_llh(path):
         for i in range(0,n):
             time = file.read(8)
             time = struct.unpack('<q', time)[0]
-            print(time)
             pos = np.zeros([3])
             mat = np.zeros([3])
             for i in range(0, 3):
@@ -37,22 +37,29 @@ def parse_binary_llh(path):
 def graph_series(series):
     series = sorted(series, key=lambda x: x.time)
     mindate = series[0].time
+    p0 = np.array(series[0].pos)
+    phi,lam,h = p0
+    mat = np.array([[-1*sin(phi)*cos(lam), -1*sin(phi)*sin(lam),cos(phi)],
+                    [-1*sin(lam),cos(lam),0],
+                    [-1*cos(phi)*cos(lam), -1*cos(phi)*sin(lam),-1*sin(phi)]])
 
     positions = []
     times = []
+    i = 0
     for elem in series:
-        positions.append(list(elem.pos))
         year = elem.time//1000
         days = elem.time - year*1000
-        print(days)
         times.append(datetime.date.fromordinal(datetime.date(year, 1, 1).toordinal() + days - 1))
+        positions.append(np.matmul(mat,elem.pos-p0))
+        pos = np.zeros([3])
+
 
     positions = np.array(positions)
     f, axarr = plt.subplots(3, sharex=True)
-    f.suptitle('Sharing X axis')
-    axarr[0].plot(times, positions[:, 0])
-    axarr[1].plot(times, positions[:, 1])
-    axarr[2].plot(times, positions[:, 2])
+    f.suptitle('DAMn')
+    axarr[0].scatter(times, positions[:, 0], s=0.01)
+    axarr[1].scatter(times, positions[:, 1], s=0.01)
+    axarr[2].scatter(times, positions[:, 2], s=0.01)
     plt.show()
 
 graph_series(parse_binary_llh(sys.argv[1]))
