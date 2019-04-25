c8e1733bf171603073f49e6d233dabe0966fd381
Vladimir fayt <vfayt99@gmail.com>
2019-04-25 11:17:59 +0200

    yo

--- graphing.py
+++ graphing.py
@@ -19,7 +19,6 @@ def parse_binary_llh(path):
     with open(path, 'rb') as file:
         n = file.read(8)
         n = struct.unpack('<q', n)[0]
-
         for i in range(0,n):
             time = file.read(8)
             time = struct.unpack('<q', time)[0]
@@ -37,58 +36,72 @@ def parse_binary_llh(path):
 
             pos_f = file.tell()
             collection.append(Sample_conv(name, time, pos, mat))
-            series = sorted(collection,key = lambda x: x.time)
- 
+        series = sorted(collection,key = lambda x: x.time)
+        # series = average_over(series, 7)
 
-            times = []
-            locx = []
-            locy = []
-            locz = []
-            init_time = series[0].time
-            init_year = init_time//1000
-            init_days = init_time - init_year*1000
-            init_date = datetime.date.fromordinal(datetime.date(init_year,1,1).toordinal()+ init_days - 1)
+
+        times = []
+        locx = []
+        locy = []
+        locz = []
+        init_time = series[0].time
+        init_year = init_time//1000
+        init_days = init_time - init_year*1000
+        init_date = datetime.date.fromordinal(datetime.date(init_year,1,1).toordinal()+ init_days - 1)
+
+        for i in range(len(series)):
+            ts = series[i].time
+            year = ts//1000
+            days = ts - year*1000
+            date = datetime.date.fromordinal(datetime.date(year,1,1).toordinal()+ days - 1)
+            time = (date - init_date).total_seconds()
+            times.append(time)
+            locx.append(series[i].pos[0])
+            locy.append(series[i].pos[1])
+            locz.append(series[i].pos[2])
             
 
-            for i in range(len(series)):
+        datax = np.column_stack((times,locx))
+        datay = np.column_stack((times,locy))
+        dataz = np.column_stack((times,locz))
 
-                ts = series[i].time
-                year = ts//1000
-                days = ts - year*1000
-                date = datetime.date.fromordinal(datetime.date(year,1,1).toordinal()+ days - 1)
-                time = (date - init_date).total_seconds()
-                times.append(time)
-                locx.append(deg(series[i].pos[0]))
-                locy.append(deg(series[i].pos[1]))
-                locz.append(deg(series[i].pos[2]))
-                
-
-            datax = np.column_stack((times,locx))
-            datay = np.column_stack((times,locy))
-            dataz = np.column_stack((times,locz))
-
-            newdatax = outlierdet(datax,300,1)
-            newdatay = outlierdet(datay,300,1)
-            newdataz = outlierdet(dataz,300,1)
-            plps = [newdatax[:,1],newdatay[:,1],newdataz[:,1]]
-            # inl_x, inl_y, outl_x, outl_y = outlierdet(times,locations,0.45)
+        newdatax = outlierdet(datax,300,1)
+        newdatay = outlierdet(datay,300,1)
+        newdataz = outlierdet(dataz,300,1)
+        plps = [newdatax[:,1],newdatay[:,1],newdataz[:,1]]
+        plts = [newdatax[:,0],newdatay[:,0],newdataz[:,0]]
+        plts2 = [newdatax[:,0]-len(newdatax)*newdatax[0,0],newdatay[:,0]-len(newdatay)*newdatay[1,0],newdataz[:,0]-len(newdataz)*newdataz[2,0]]
+        # inl_x, inl_y, outl_x, outl_y = outlierdet(times,locations,0.45)
 
 
 
-            north = linregress(times,newdatax[:, 1])
-            east = linregress(times, newdatay[:, 1])
-            up, away = curve_fit(to_fit, times,newdataz[:, 1])
-            print("{} mm/y".format(north[0]*365*1000))
-            print("{} mm/y".format(east[0]*365*1000))
-            print("{} mm/y".format(up[1]*365*1000))
-            print(north)
+        north = linregress(plts2[0],newdatax[:, 1])
+        print("north", north)
+        input()
+        east = linregress(plts2[1], newdatay[:, 1])
+        up, away = curve_fit(to_fit, plts2[2],newdataz[:, 1])
+        print("{} mm/y".format(north[0]*365*1000))
+        print("{} mm/y".format(east[0]*365*1000))
+        print("{} mm/y".format(up[1]*365*1000))
+        print(north)
         f, gra = plt.subplots(3, sharex=True)
         f.suptitle('lat, lon, heigth')
 
+        p0 = np.array([datax[0,1],datay[0,1],dataz[0,1]])
+        phi, lam, h = p0
+
+        mat = np.array([[-1*sin(lam),cos(lam), 0.],
+                            [-1*cos(lam)*sin(phi), -1*sin(lam)*sin(phi), cos(phi)],
+                            [cos(lam)*cos(phi), sin(lam)*cos(phi), sin(phi)]]).T
+
         for i in range(0,3):
             gra[i].axhline(y=0, color='k')
-            gra[i].set_xlim([times[0], times[-1]])
-            gra[i].plot(times, plps[i], linewidth=0.5)
+            gra[i].set_xlim([plts[i][0], plts[i][-1]])
+            pos = []
+            for j in range(len(plps[i])):
+                temp = np.array(plps[i][j])-np.array(p0[i])
+                pos.append(mat[0,i]*temp + mat[1,i]*temp + mat[2,i]*temp)
+            gra[i].plot(plts[i], pos, linewidth=0.5)
 
         # plt.subplot(3,1,1)
         # plt.scatter(newdatax[:,0],newdatax[:,1],s = 2)
@@ -100,9 +113,9 @@ def parse_binary_llh(path):
         # plt.subplot(3,1,3)
         # plt.scatter(newdataz[:,0],newdataz[:,1],s = 2)
         # plt.ylim(min(newdataz[:,1]),max(newdataz[:,1]))
-        axarr[0].plot([mindate, times[-1]], [north[1], north[1] + north[0]*times[-1]])
-        axarr[1].plot([mindate, times[-1]], [east[1], east[1] + east[0]*times[-1]])
-        axarr[2].plot(times, list(map(lambda x: to_fit(x, up[0], up[1], up[2], up[3]), times)))
+        # gra[0].plot([plts[0][0], plts[0][-1]], [north[1], north[1] + north[0]*plts2[0][-1]])
+        # gra[1].plot([plts[1][0], plts[1][-1]], [east[1], east[1] + east[0]*plts2[1][-1]])
+        # gra[2].plot(plts[2][:], list(map(lambda x: to_fit(x, up[0], up[1], up[2], up[3]), plts2[2])))
         
         plt.show()
         return collection
