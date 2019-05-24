bac26ef7156b037bd0c4f0597d3b7864a335dfbd
Vladimir fayt <vfayt99@gmail.com>
2019-05-24 10:25:00 +0200

    yo

--- new_parse.py
+++ new_parse.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import sys
 import matplotlib.dates as mdates
 from scipy.stats import linregress
+from scipy.optimize import curve_fit
+import numpy as np
 
 def parse_date(string):
 	year = string[-2:]
@@ -52,15 +54,34 @@ for i in range(len(lat_file)):
 
 f, axarr = plt.subplots(3, sharex=True)
 
+baseline = points[0].time
+split = datetime.datetime(2005,7,26)
+splitindex = 0
+for i in range(len(points)):
+    if points[i].time < split:
+        splitindex = i
+    else:
+        break
+
+f.suptitle(sys.argv[1])
 years = mdates.YearLocator()   # every year
 months = mdates.MonthLocator()  # every month
 yearsFmt = mdates.DateFormatter('%Y')	
 print(type(points[0].time))
 
-for i in range(3):
+def trend(x, a, b, c, d):
+	return a/x + b*x + c
+
+for i in range(2):
 	predict = linregress([(x.time-points[0].time).days for x in points], [x.pos[i] for x in points])
+	p, cov = curve_fit(trend, [(x.time-points[0].time).days for x in points[splitindex+1:]], [x.pos[i] for x in points[splitindex+1:]])
+	axarr[i].axvline(x=datetime.datetime(2004,12,26))
 	axarr[i].axhline(y=0, color='k')
 	axarr[i].set_xlim([min([x.time for x in points]), max([x.time for x in points])])
 	axarr[i].errorbar([x.time for x in points], [x.pos[i] for x in points], yerr=[x.err[i] for x in points], fmt='x', elinewidth=0.1, markersize=0.55)
-	axarr[i].plot([points[0].time, points[-1].time], [predict[1], predict[1]+predict[0]*(points[-1].time-points[0].time).days])
+	#axarr[i].plot([points[0].time, points[-1].time], [predict[1], predict[1]+predict[0]*(points[-1].time-points[0].time).days])
+	axarr[i].plot([x.time for x in points[splitindex+1:]], [trend((x.time-points[0].time).days, *p) for x in points[splitindex+1:]])
+
+
+
 plt.show()
